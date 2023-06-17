0a7ce06d3531838e204175a582815c7323da1a1d
Bashiul Alam Sabab <bashiul.alam@gmail.com>
2023-06-17 12:59:38 +0200

    redis clean up functionality, corresponding tests and schema validation for create api response added

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import redis
+
 from tavern.core import run
 
+def clear_redis_cache():
+    r = redis.Redis()
+    r.flushdb()
 def run_tests():
+    print("\n\nExecuting API tests...\n\n")
     run("test_weather-api.tavern.yaml", tavern_global_cfg={})
+
+def run_empty_redis_validation_tests():
+    print("\n\nExecuting empty redis validation tests...\n\n")
+    run("test_empty-redis-cache-validation.tavern.yaml", tavern_global_cfg={})
 
 if __name__ == "__main__":
     run_tests()
+    clear_redis_cache()
+    run_empty_redis_validation_tests()
